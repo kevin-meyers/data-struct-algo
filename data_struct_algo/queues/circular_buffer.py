b65e020ed4ef282350a5a854c42d0ee5df907300
Kevin <kevinm1776@gmail.com>
2019-12-10 17:11:54 -0800

    Fixed empty() and hashing caused many problems.

--- data_struct_algo/queues/circular_buffer.py
+++ data_struct_algo/queues/circular_buffer.py
@@ -1,9 +1,10 @@
 
 
 class CircularBuffer(list):
-    def __init__(self, fixed_size):
+    def __init__(self, fixed_size, initial_value=None):
         self.max_size = fixed_size
-        self.extend([None for _ in range(self.max_size)])
+        self.initial_value = initial_value
+        self.extend([self.initial_value for _ in range(self.max_size)])
         self.head = 0
         self.length = 0
 
@@ -14,16 +15,13 @@ class CircularBuffer(list):
         while len(self) > 0:
             yield self.dequeue()
 
-    def __repr__(self):
+    def __str__(self):
         items = []
         while len(items) < len(self):  # shows all items
             item = self[(self.head + len(items)) % self.max_size]
             items.append(str(item))
 
         return ' '.join(items)
-
-    def __hash__(self):
-        return hash(repr(self))
 
     def enqueue(self, data):
         index = (self.head + len(self)) % self.max_size
@@ -57,13 +55,16 @@ class CircularBuffer(list):
         return item
 
     def empty(self):
-        [_ for _ in self]
+        ''' Calls init to reset object. '''
+        self.__init__(self.max_size, self.initial_value)
 
 
 if __name__ == '__main__':
     cb = CircularBuffer(3)
     for num in range(4):  # 0 should get overwritten
         cb.enqueue(num)
+
+    cb.empty()
 
     for num in cb:
         print(num)
